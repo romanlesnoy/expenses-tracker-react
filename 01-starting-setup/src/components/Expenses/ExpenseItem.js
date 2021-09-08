a6d149e1d54465d56f7968891bef15d5bacc669a
Roman Lesnoy <romanlesnoy@outlook.com>
2021-09-08 14:29:47 +0300

    add NewExpense components

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,25 @@
-import ExpenseDate from "./ExpenseDate";
-import Card from "../UI/Card";
-import "./ExpenseItem.css";
+import React, {useState} from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+    const [title, setTitle] = useState(props.title); 
+    
+    function clickHandler() {
+        setTitle('Updated?');
+        console.log(title);
+    }
+    
     return (
-        <Card className="expense-item">
+        <Card className='expense-item'>
             <ExpenseDate date={props.date} />
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <p className="expense-item__price">${props.amount}</p>
+            <div className='expense-item__description'>
+                <h2>{title}</h2>
+                <p className='expense-item__price'>${props.amount}</p>
             </div>
+            <button onClick={clickHandler}>Change title</button>
         </Card>
     );
 }
--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
-import ExpenseItem from "./ExpenseItem";
-import Card from "../UI/Card";
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
 import './Expenses.css';
 
 function Expenses(props) {
